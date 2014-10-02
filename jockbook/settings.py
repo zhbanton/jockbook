23e09180d9d699d7ea39ec00d676624ae9595660
zhbanton <zhbanton@gmail.com>
2014-10-02 19:37:56 -0400

    added athletes app, configured db

--- jockbook/settings.py
+++ jockbook/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'jockbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jockbook',
+        'USER': 'zackbanton',
+        'PASSWORD': 'zhb123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
